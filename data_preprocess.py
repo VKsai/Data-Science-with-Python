13b713e0e09e7598ada0a58c96eedc37e49c12c8
VKsai <svedula2@horizon.csueastbay.edu>
2016-12-19 09:38:30 -0800

    Add files via upload

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,18 +1,21 @@
+#Data Preprocessing Using sklearn libraries
 #importing required libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import LabelEncoder
 
-#reading data
+#READING DATA
 #Type your data file name replacing <data file name>
 #In this case it is Comma Sepearated Values, it can be other files like 
 #excel file, et cetera.
-data = pd.read_csv('<data file name>');
+data = pd.read_csv('Data.csv');
 X = data.iloc[:, :-1].values #: is for all elements and -1 is for removing last column
 Y = data.iloc[:, 3].values # [:, :] == [rows, columns]
 
-#using sklearn's imputer to cement up the missing values
+#using sklearn's imputer to cement up the MISSING VALUES
+#Defined in line 6
 #The default strategy for filling up the missing value is to fill NAN's with
 #respective column's mean.
 #use sklearn's Imputer class with the respective parameters
@@ -22,13 +25,24 @@ Y = data.iloc[:, 3].values # [:, :] == [rows, columns]
 # "nan" or blank spaces with respective means of the column.
 i = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 
+#FITTING or INSERTING UPDATED VALUES TO DESIRED MATRIX
 #Now that the Imputer is called we need to fit the means into their respective positions
 #in the data set.
 #The X matrix with the columns which needs to be transformed are passed. 
 iputer_fit = i.fit(X[:, 1:3])
 
+#TRANSFORMING THE MATRIX
 #Applying Imputer's transform method with X as parameter and hence updating X.
 X[:, 1:3] = i.transform(X[:, 1:3])
 
-#Printing X you can see how the spaces are replaced by respective means.
-print(X)
+#BRINGING LABELS DOWN TO NUMBERS
+#Once the missing values or nan's are replaced using a prticular strategy, we can then 
+#work on categorizing data to some reconizable format
+#Let us define the required library at line 7
+labelEncoder_variable = LabelEncoder()
+
+#FITTING NUMBERED DATA
+#Now that we have initiated the object of LabelEncoder in line 39
+#Next we need to fit that into the matrix X
+X[:, 0] = labelEncoder_variable.fit_transform(X[:, 0])
+
